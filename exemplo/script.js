5b67170d176bb3c1ad21d78abf89a7ffacc75c1c
Jussiani da Silva Machado <jussianidasilvamachado075@gmail.com>
2024-11-11 19:53:30 -0300

    pasta ifiber

---
+++ exemplo/script.js
@@ -0,0 +1,11 @@
+var curso = "TESTE"
+alert(curso)
+
+/* 
+operadores aritmeticos: + - * / () **
+operadores relacionais|: < > >= <= ='atribuição' =='comparação' ==='comparação tipos'
+operadores logicos: !negação ||ou &&E
+operadores booleanos: true - verdaderio false - falso
+ */
+
+      
